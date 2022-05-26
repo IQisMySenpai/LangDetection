762aa6dae612e24a310ccd74a2740eae8a9ff0d2
AliSot2000 <44428989+AliSot2000@users.noreply.github.com>
2022-05-26 20:47:49 +0200

    Update recognition.py
    Added Dataclass for better usability.

--- recognition.py
+++ recognition.py
@@ -1,4 +1,32 @@
+from dataclasses import dataclass
 import os
+
+
+@dataclass()
+class LanguageDetection:
+    language: str
+    matches: int
+    probability: float
+
+    def __cmp__(self, other):
+        if self.probability < other.probability:
+            return -1
+        elif self.probability > other.probability:
+            return 1
+        else:
+            return 0
+
+    def __le__(self, other):
+        return self.probability <= other.probability
+
+    def __lt__(self, other):
+        return self.probability < other.probability
+
+    def __gt__(self, other):
+        return self.probability > other.probability
+
+    def __ge__(self, other):
+        return self.probability >= other.probability
 
 
 class Recognition:
@@ -16,30 +44,33 @@ class Recognition:
 
     def detect(self, text):
         text = text.lower()
-        score = {}
-        total_words = 0
+        score = []
+        total_chars = len(text)
 
         for lang in self.words:
-            score[lang] = {'matches': 0}
+            lang_chars = 0
+            cur_lang = LanguageDetection(language=lang, matches=0, probability=0)
 
             spaced = self.langs[lang]
 
             for word in self.words[lang]:
                 if spaced:
-                    count = text.count(f'{word} ')
-                else:
-                    count = text.count(word)
-                score[lang]['matches'] += count
+                    word = f'{word} '
+                count = text.count(word)
 
-            total_words += score[lang]['matches']
+                cur_lang.matches += count
 
-        for lang in self.words:
-            if total_words > 0:
-                score[lang]['probability'] = score[lang]['matches']/total_words
-            else:
-                score[lang]['probability'] = 0
+                lang_chars += count * len(word)
 
-        return dict(sorted(score.items(), key=lambda item: item[1]['probability'], reverse=True))
+            cur_lang.probability = lang_chars/total_chars
+            score.append(cur_lang)
+
+        # for i in range(len(score)):
+        #     if total_chars > 0:
+        #         score[i].probability = score[i].matches/total_chars
+
+        # return dict(sorted(score.items(), key=lambda item: item[1]['probability'], reverse=True))
+        return sorted(score, reverse=True)
 
 
 def lower_text (fp, lang):
